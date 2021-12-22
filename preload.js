30df39e9fcbbe161e77c20d20cab7996f429b669
FacundoMendez <facundo.mendez@live.com.ar>
2021-12-22 14:40:07 -0300

    cambios en el home

---
+++ preload.js
@@ -0,0 +1,9 @@
+(() =>{
+    let preload = document.querySelector(".preload");
+
+    setTimeout(function(){
+        preload.classList.add("cerrar");
+        preload.style.zIndex=0;
+    },3000)
+    
+})();
